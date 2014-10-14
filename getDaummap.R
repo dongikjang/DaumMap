41e6d531c180ebb628f328a7ac7394121091d9b3
dongikjang <dongik.s.jang@gmail.com>
2014-10-14 09:26:17 +0900

    Update getDaummap.R

--- getDaummap.R
+++ getDaummap.R
@@ -5,7 +5,8 @@ toTileDaum <- function(lon, lat, zoom=NA, maproj = c("WGS84", "Daum")[2]){
     if(!is.na(zoom)){
       if(zoom > maxZoomLevels) stop(paste("zoom level is greater than", maxZoomLevels, "\n"))
     }
-    
+    if(length(lon) > 2) lon <- range(lon, na.rm=TRUE)
+    if(length(lat) > 2) lat <- range(lat, na.rm=TRUE)
       
     xy <- data.frame(x=lon, y=lat)
     coordinates(xy) <- c("x", "y")
